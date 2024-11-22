2c9e0cb11c43bcb262eb9c15fc4dfecb037f1631
simongig <s.giglhuber@gmx.de>
2024-11-22 17:38:37 +0100

    routers adjustment for API

--- backend/routers.py
+++ backend/routers.py
@@ -1,7 +1,8 @@
 from rest_framework import routers
-from testdata.viewsets import SpeiseViewSet, OrderItemViewSet
+from testdata.viewsets import SpeiseViewSet, OrderItemViewSet, MesseEventViewSet
 
 
 router = routers.SimpleRouter()
 router.register(r'speise', SpeiseViewSet, basename='speise')
 router.register(r'order', OrderItemViewSet, basename='order')
+router.register(r'messeevent', MesseEventViewSet, basename='messeevent')
