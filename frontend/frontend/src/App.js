b14598a467646712a984892c381ca2f2ac572e84
AntoniaSperling <antonia.sperling@stundent.htw.-berlin.de>
2024-11-26 15:22:58 +0100

    git init

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MyDashboard from './components/Dashboard';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MyDashboard />
+    </div>
+  );
+}
+
+
+export default App;
