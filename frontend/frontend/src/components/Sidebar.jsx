4592fbc886ad54db25c43823baf7bdaf973aad9e
AntoniaSperling <antonia.sperling@stundent.htw.-berlin.de>
2024-11-29 11:10:10 +0100

    Anpassung der Farbe und der Schriftart

--- frontend/frontend/src/components/Sidebar.jsx
+++ frontend/frontend/src/components/Sidebar.jsx
@@ -7,6 +7,7 @@ const Sidebar = () => {
       <div className="logo">
         <h2>Your Logo</h2>
       </div>
+      <div style={{width: '100%', border: '1px #1D1E2C solid'}}></div>
       <ul className="nav-list">
         <li className="nav-item active">
           <i className="icon">⌛</i> Dashboard
@@ -24,9 +25,11 @@ const Sidebar = () => {
           <i className="icon">📊</i> Verkaufszahlen
         </li>
       </ul>
+      <div style={{width: '100%', border: '1px #1D1E2C solid'}}></div>
       <div className="bottom-links">
         <div className="nav-item"><i className="icon">⚙️</i> Settings</div>
         <div className="nav-item"><i className="icon">❓</i> Support</div>
+        <div style={{width: '100%', border: '1px #1D1E2C solid'}}></div>
         <div className="nav-item logout"><i className="icon">🔓</i> Log out</div>
       </div>
     </div>
