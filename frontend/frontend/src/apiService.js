4f9cf0feb6d2be64b061a9abff93bdb69a03ca84
AntoniaSperling <antonia.sperling@stundent.htw.-berlin.de>
2024-12-10 12:55:08 +0100

    Erster Versuch BE-Anbindung

---
+++ frontend/frontend/src/apiService.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const API_BASE_URL = '	http://localhost:8000/api';
+
+export const fetchTotalEarnings = async () => {
+    try {
+        const response = await axios.get(`${API_BASE_URL}/total_earnings`);
+        // Extrahiere den Wert für "total" aus dem JSON
+        return response.data.earnings.total;
+    } catch (error) {
+        console.error('Error fetching total earnings:', error);
+        throw error;
+    }
+};
