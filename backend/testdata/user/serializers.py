72fc7dbb0998629ec19f8152a1f1784a955079c4
simongig <simongig@proton.me>
2024-12-13 00:00:58 +0100

    fix user creation clear password

--- backend/testdata/user/serializers.py
+++ backend/testdata/user/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+        )
+
+        return user
+
     class Meta:
         model = User
-        fields = '__all__'
+        # Tuple of serialized model fields (see link [2])
+        fields = ( "id", "username", "password", )
--- backend/testdata/user/viewsets.py
+++ backend/testdata/user/viewsets.py
@@ -1,11 +1,13 @@
 from testdata.user.serializers import UserSerializer
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
+User = get_user_model()
+
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
-    permission_classes = (IsAuthenticated,)
+    # permission_classes = (IsAuthenticated,)
     
     def get_queryset(self):
         return User.objects.all()
